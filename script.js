fd2b8c8b26402567b1ce00c231effc30ca888e9f
F4RUII <faruii2301@gmail.com>
2025-12-30 19:47:47 +0700

    polished before publishhhhh

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const submit = document.getElementById('submit');
 
-const urlElement = document.getElementById('url');
+const countElement = document.getElementById('count');
 const idElement = document.getElementById('id');
 
 let currentId = 100000;
+let currentCount = 0;
 let running = false;
 
 submit.addEventListener('click', async () => {
@@ -12,13 +13,15 @@ submit.addEventListener('click', async () => {
     running = true;
 
     const id = parseFloat(idElement.value);
+    const count = parseFloat(countElement.value);
     // Check if user has left any inputs blank, if so return and alert user to fill the inputs
-    if (id === '') {
+    if (count === '' || id === '') {
         alert('Please fill in the necessary information!');
         return;
     };
 
     currentId += id;
+    currentCount += count;
 
     // do stuff
     await operate();
@@ -28,11 +31,11 @@ async function operate() {
     console.log(running);
     console.log(currentId);
     
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < currentCount; index++) {
         const currentcurrentId = currentId + index;
         polishedId = currentcurrentId.toString().slice(1, currentcurrentId.length);
 
-        window.open(`https://www.w3schools.com/tags/${polishedId}`);
+        window.open(`https://cdn.xn--l3cjg1aa9gm3hwc.com/034/segment_${polishedId}`);
         
         await delay(500);
         console.log(`${polishedId}, delayed ${index}`);
